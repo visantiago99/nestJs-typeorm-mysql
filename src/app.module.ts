94215a2bf02377a250da3cee5df6dab0218d6734
Victor <czpask@gmail.com>
2023-11-10 11:22:43 -0300

    Add dotenv and create users module, controller and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
+import { UsersModule } from './users/users.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Module({
   imports: [
@@ -9,12 +13,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: '',
-      password: '',
+      username: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
       database: 'typeorm_schema_test',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
